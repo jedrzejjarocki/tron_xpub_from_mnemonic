0af092a77fd786556dc0e5fcd1084fe3b7d18674
Jędrzej Jarocki <jedrzej.jarocki@inpay.pl>
2023-08-09 15:44:30 +0200

    get mnemonic phrase from .env file

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 "use strict";
+require('dotenv').config();
 
-const args = process.argv.slice(2);
-const mnemonic = args[0] || null;
+const MNEMONIC = process.env.MNEMONIC || null;
 
 const bip39 = require("bip39");
-if(mnemonic) {
-  const isValid = bip39.validateMnemonic(mnemonic, bip39.EN);
+if(MNEMONIC) {
+  const isValid = bip39.validateMnemonic(MNEMONIC, bip39.EN);
   if(!isValid) {
     console.error("Invalid mnemonic (EN wordlist)");
     return;
   }
+} else {
+  console.log("No mnemonic provided, generating one...");
 }
 
 const tron = require("@tatumio/tron");
 const sdk = tron.TatumTronSDK({ apiKey: '' });
-const wallet = sdk.wallet.generateWallet(mnemonic);
+const wallet = sdk.wallet.generateWallet(MNEMONIC);
 wallet.then(console.log)
-
